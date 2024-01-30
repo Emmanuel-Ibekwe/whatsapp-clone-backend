a8111439e5c6ed66c350f0a2fc44f024231cc503
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-01-30 11:59:44 +0100

    completed the signup controller

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,56 @@
+import dotenv from "dotenv";
+import { createUser } from "../services/auth.js";
+import { generateToken } from "../services/token.js";
+
+dotenv.config();
 export const signup = async (req, res, next) => {
   try {
-    res.send(req.body);
+    const { name, email, status, picture, password } = req.body;
+
+    const newUser = await createUser({
+      name,
+      email,
+      status,
+      picture,
+      password
+    });
+
+    const access_token = await generateToken(
+      {
+        userId: newUser._id.toString(),
+        email: newUser.email
+      },
+      "1d",
+      process.env.ACCESS_TOKEN_SECRET
+    );
+
+    const refresh_token = await generateToken(
+      {
+        userId: newUser._id.toString(),
+        email: newUser.email
+      },
+      "30d",
+      process.env.REFRESH_TOKEN_SECRET
+    );
+
+    res.cookie("refreshtoken", refresh_token, {
+      httpOnly: true,
+      path: "api/v1/auth/refreshtoken",
+      maxAge: 30 * 24 * 60 * 60 * 1000 //30 days
+    });
+
+    res.status(201).json({
+      message: "signup successful.",
+      access_token,
+      user: {
+        _id: newUser._id,
+        name: newUser.name,
+        email: newUser.email,
+        picture: newUser.picture,
+        status: newUser.status,
+        token: access_token
+      }
+    });
   } catch (error) {
     if (!error.status) {
       error.status = 500;
