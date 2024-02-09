de14bb9ff7e329110637b3052c0ac990862677f0
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-09 15:49:51 +0100

    chore: imported the conversation routes to the index route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import authRoutes from "./auth.js";
+import conversationRoutes from "./conversation.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/conversation", conversationRoutes);
 
 export default router;
