735c6e8e144a1c8b6ecaef8b62e1b720fcdf2ed0
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-09 15:45:27 +0100

    feat: Added the conveersation routes

---
+++ src/routes/conversation.js
@@ -0,0 +1,15 @@
+import express from "express";
+import trimRequest from "trim-request";
+import authMiddleware from "../middlewares/authMiddleware.js";
+import {
+  create_open_conversation,
+  getConversations,
+  createGroup
+} from "../controllers/conversation.js";
+const router = express.Router();
+
+router.post("/", trimRequest.all, authMiddleware, create_open_conversation);
+router.get("/", trimRequest.all, authMiddleware, getConversations);
+router.post("/group", trimRequest.all, authMiddleware, createGroup);
+
+export default router;
