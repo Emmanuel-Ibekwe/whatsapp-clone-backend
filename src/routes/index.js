3bd0cd33026f2bd090bb001ad3191a642ef1b06d
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-10 13:28:34 +0100

    chore: Added the message routes to the index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 import express from "express";
 import authRoutes from "./auth.js";
 import conversationRoutes from "./conversation.js";
+import messageRoutes from "./message.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
 router.use("/conversation", conversationRoutes);
+router.use("/message", messageRoutes);
 
 export default router;
