361a5585a38b6bde0053e4b6f86203d7b10d94ac
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-15 13:45:03 +0100

    feat: Added the user controller file

---
+++ src/controllers/user.js
@@ -0,0 +1,19 @@
+import createHttpError from "http-errors";
+import { regexSearchUsers } from "../services/user.js";
+
+export const searchUsers = async (req, res, next) => {
+  try {
+    const keyword = req.query.search;
+    if (!keyword) {
+      throw createHttpError.BadRequest("search query not provided.");
+    }
+
+    const users = await regexSearchUsers(keyword);
+    res.status(200).json(users);
+  } catch (error) {
+    if (!error.status) {
+      error.status = 500;
+    }
+    next(error);
+  }
+};
