d265dab92c8a7f1dd67103a17cbdc215e78a22d6
Emmanuel Ibekwe <emmanuelibekwe7@gmail.com>
2024-02-15 13:50:54 +0100

    chore: Added userRoutes to the index route file

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,10 +2,13 @@ import express from "express";
 import authRoutes from "./auth.js";
 import conversationRoutes from "./conversation.js";
 import messageRoutes from "./message.js";
+import userRoutes from "./user.js";
 
 const router = express.Router();
 
 router.use("/auth", authRoutes);
+router.use("/user", userRoutes);
+
 router.use("/conversation", conversationRoutes);
 router.use("/message", messageRoutes);
 
